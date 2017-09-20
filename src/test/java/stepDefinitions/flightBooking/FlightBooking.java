75368cae2184ccbc8cda0c78c435037c2824c896
Ash Modi <ashish@clearscore.com>
2017-09-20 23:59:16 +0100

    Removed unwanted imports.

--- src/test/java/stepDefinitions/flightBooking/FlightBooking.java
+++ src/test/java/stepDefinitions/flightBooking/FlightBooking.java
@@ -1,11 +1,9 @@
 package stepDefinitions.flightBooking;
 
 import cucumber.api.DataTable;
-import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.support.PageFactory;
 import pageObjects.*;
